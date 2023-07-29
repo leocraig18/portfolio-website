b6bb2508b2bd4c3f9609f3e1dfea9f80301c8dfc
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-07-29 17:50:50 +0100

    Age variable, particles, refactoring for modularity

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    date = datetime.now()
+    dob = datetime(2001, 4, 18)
+    age = date.year - dob.year - ((date.month, date.day) < (dob.month, dob.day))
+    return render_template('index.html', age=age)
 
 
 @app.route('/resume')
