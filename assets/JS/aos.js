507939fe11393c4795b239f86827a691bab7d8d6
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-08-10 16:23:06 +0100

    Remove AOS on phone

--- assets/JS/aos.js
+++ assets/JS/aos.js
@@ -1,13 +1,19 @@
-// // OR IF ONLY WANTING TO APPLY THE ANIMATION ONCE:
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-      observer.unobserve(entry.target);
-    }
+const minWidthForAnimation = 768;
+
+if (window.innerWidth >= minWidthForAnimation) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
   });
-});
 
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((element) => observer.observe(element));
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((element) => observer.observe(element));
+} else {
+  // For mobile devices or smaller screens, remove the .hidden class to show the elements directly
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach((element) => element.classList.remove("hidden"));
+}
