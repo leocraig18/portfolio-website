c85c53a3aeb18be30dfbb115900d425b32f4147a
leocraig18 <110471317+leocraig18@users.noreply.github.com>
2023-08-24 23:59:19 +0100

    Refactor JS

---
+++ assets/JS/typed.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".auto-type", {
+  strings: ["Leo", "an Economics Graduate and Full Stack Developer"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  smartBackspace: true,
+  loop: true,
+});
